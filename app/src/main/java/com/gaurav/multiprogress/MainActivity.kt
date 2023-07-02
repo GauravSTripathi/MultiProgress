db8ef7e0a2d1cebd948780965641f68bc0241335
GauravSTripathi <info.gstripathi@gmail.com>
2023-07-02 11:39:27 +0530

    Bug solved In Day night hours Add corner radius of progress

--- app/src/main/java/com/gaurav/multiprogress/MainActivity.kt
+++ app/src/main/java/com/gaurav/multiprogress/MainActivity.kt
@@ -16,33 +16,26 @@ class MainActivity : AppCompatActivity() {
 
         var pair = ArrayList<Pair<Float, Float>>()
 
-//        pair.add(Pair(8F,10F))
-//        pair.add(Pair(12F,17F))
-//        pair.add(Pair(18F,19F))
-//        pair.add(Pair(21F,23F))
-
-
-        pair.add(Pair(0F, 2F))
-        pair.add(Pair(3F, 4F))
-        pair.add(Pair(6F, 8F))
-////        pair.add(Pair(21F,23F))
-
+        pair.add(Pair(8F, 10F))
+        pair.add(Pair(12F, 17F))
+        pair.add(Pair(18F, 19F))
+        pair.add(Pair(21F, 23F))
+        binding.multiProgressTaskOne.setHours(8F, 23F)
         binding.multiProgressTaskOne.setProgress(pair)
-
+        binding.multiProgressTaskOne.progress_radius = 50F
+//
         pair = ArrayList()
-        pair.add(Pair(1F, 3F))
-        pair.add(Pair(5F, 7F))
-//        pair.add(Pair(6F,8F))
-////        pair.add(Pair(21F,23F))
-
+        pair.add(Pair(0F, 0.7F))
+        pair.add(Pair(5F, 5.2F))
+        binding.multiProgressTaskTwo.setHours(0F, 7F)
         binding.multiProgressTaskTwo.setProgress(pair)
 
         pair = ArrayList()
-        pair.add(Pair(8F, 12F))
-//        pair.add(Pair(3F,4F))
         pair.add(Pair(16F, 19F))
-////        pair.add(Pair(21F,23F))
+        pair.add(Pair(22F, 5F))
+        pair.add(Pair(6F, 8F))
 
+        binding.multiProgressTaskThree.setHours(16F, 8F)
         binding.multiProgressTaskThree.setProgress(pair)
 
 
